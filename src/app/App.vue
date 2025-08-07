b4f0cdbca2503db93f7f26ab6ead4a16f8e77ba0
Victor <viktormurzinwork@gmail.com>
2025-08-07 17:49:27 +0700

    feat: init project

---
+++ src/app/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import MainPage from '../pages/MainPage.vue'
+</script>
+
+<template>
+  <MainPage />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family:
+    -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+}
+
+#app {
+  width: 100%;
+  height: 100dvh;
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <div class="sidebar">
+      <h2>Сайдбар</h2>
+    </div>
+    <div class="mapContainer">
+      <h2>Карта</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+.sidebar {
+  width: 30%;
+  background-color: #f5f5f5;
+  padding: 20px;
+  border-right: 1px solid #ddd;
+}
+
+.mapContainer {
+  width: 70%;
+  padding: 20px;
+  background-color: #fff;
+}
+
+h2 {
+  margin: 0;
+  color: #333;
+}
+</style>
